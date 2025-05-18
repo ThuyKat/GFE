a94565c5b9a7f6673c47a28aaf4875af2546b5a4
ThuyKat <katiefirst7@gmail.com>
2025-05-18 23:49:04 +1000

    question solved : implement array.prototype.reduce

---
+++ Array.prototype.reduce.js
@@ -0,0 +1,3 @@
+/*
+Implement Array.prototype.reduce. To avoid overwriting the actual Array.prototype.reduce which is being used by the autograder, we shall instead implement it as Array.prototype.myReduce.
+*/
