0e331c0887761d94418ca9105c904ba0345703d5
ThuyKat <katiefirst7@gmail.com>
2025-05-20 23:49:39 +1000

    UI question added

--- JavaScript/Array.prototype.reduce.js
+++ JavaScript/Array.prototype.reduce.js
@@ -1,7 +1,12 @@
 /*
 Implement Array.prototype.reduce. To avoid overwriting the actual Array.prototype.reduce which is being used by the autograder, we shall instead implement it as Array.prototype.myReduce.
 */
+/*
+This question might seem easy on first glance, but the nuances make the question trickier than it seems on the surface. Knowing the nuances differentiates senior candidates and gives you bonus points. Are you aware that:
 
+The reducer callback is passed the currentIndex and array as the third and fourth argument respectively?
+If there is no initial value supplied to the reduce function, the array element at index 0 is used and the iteration starts from the next element (index 1 instead of index 0).
+*/
 Array.prototype.myReduce = function (callbackFn, initialValue) {
     const arr = this;
     if(initialValue === undefined && arr.length==0){
